47cbb39450febf7e4a2380fe2f27f3df6cf1b27c
ednc-jwhan <ednc-jwhan@192.168.10.161>
2017-09-20 11:13:37 +0900

    1

---
+++ src/main/java/com/ednc/DataCrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.ednc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCrawlerApplication.class, args);
+	}
+}
